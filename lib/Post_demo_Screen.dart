bf95d2309617311329c0f552a77859f750dd64cf
Nickolay82 <photon5.ufa@gmail.com>
2022-01-30 12:35:11 +0500

    добавлен фон и лого

--- lib/Post_demo_Screen.dart
+++ lib/Post_demo_Screen.dart
@@ -6,7 +6,6 @@ import 'package:http/http.dart' as http;
 Future<Post> fetchPost() async {
   final response = await http
       .get(Uri.parse('https://jsonplaceholder.typicode.com/posts/1'));
-
   if (response.statusCode == 200) {
     // If the server did return a 200 OK response,
     // then parse the JSON.
